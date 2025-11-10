3b9063adfaefe35f070b34148780fb056f80f223
essebaiyayaa <essebaiyaya@gmail.com>
2025-11-10 22:23:04 +0100

    Add AjouterMedicament form with UI, logic, and resources

---
+++ GestionPharmacie/AjouterMedicament.Designer.cs
@@ -0,0 +1,391 @@
+﻿namespace GestionPharmacie
+{
+    partial class AjouterMedicament
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges19 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges20 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges21 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges22 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges23 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges24 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges25 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges26 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges27 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges28 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges29 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges30 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges31 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges32 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges33 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges34 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges35 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges36 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            txtnom = new Guna.UI2.WinForms.Guna2TextBox();
+            txtdescription = new Guna.UI2.WinForms.Guna2TextBox();
+            txtreference = new Guna.UI2.WinForms.Guna2TextBox();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            txtprix = new Guna.UI2.WinForms.Guna2TextBox();
+            txtquantite = new Guna.UI2.WinForms.Guna2TextBox();
+            txtseuil = new Guna.UI2.WinForms.Guna2TextBox();
+            txtdate = new Guna.UI2.WinForms.Guna2DateTimePicker();
+            Retour = new Guna.UI2.WinForms.Guna2Button();
+            Enregistrer = new Guna.UI2.WinForms.Guna2Button();
+            labMessage = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 21F, FontStyle.Bold);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(407, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(413, 47);
+            label1.TabIndex = 0;
+            label1.Text = "Ajouter un Médicament";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11F);
+            label2.Location = new Point(87, 253);
+            label2.Name = "label2";
+            label2.Size = new Size(190, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Nom du Médicament";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11F);
+            label3.Location = new Point(87, 176);
+            label3.Name = "label3";
+            label3.Size = new Size(95, 25);
+            label3.TabIndex = 2;
+            label3.Text = "Référence";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 11F);
+            label4.Location = new Point(87, 315);
+            label4.Name = "label4";
+            label4.Size = new Size(108, 25);
+            label4.TabIndex = 3;
+            label4.Text = "Description";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 11F);
+            label5.Location = new Point(650, 176);
+            label5.Name = "label5";
+            label5.Size = new Size(87, 25);
+            label5.TabIndex = 4;
+            label5.Text = "Prix (DH)";
+            // 
+            // txtnom
+            // 
+            txtnom.BorderRadius = 7;
+            txtnom.CustomizableEdges = customizableEdges19;
+            txtnom.DefaultText = "";
+            txtnom.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtnom.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtnom.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtnom.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtnom.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtnom.Font = new Font("Segoe UI", 9F);
+            txtnom.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtnom.Location = new Point(300, 176);
+            txtnom.Margin = new Padding(3, 4, 3, 4);
+            txtnom.Name = "txtnom";
+            txtnom.PlaceholderText = "";
+            txtnom.SelectedText = "";
+            txtnom.ShadowDecoration.CustomizableEdges = customizableEdges20;
+            txtnom.Size = new Size(309, 36);
+            txtnom.TabIndex = 5;
+            // 
+            // txtdescription
+            // 
+            txtdescription.BorderRadius = 7;
+            txtdescription.CustomizableEdges = customizableEdges21;
+            txtdescription.DefaultText = "";
+            txtdescription.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtdescription.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtdescription.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtdescription.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtdescription.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtdescription.Font = new Font("Segoe UI", 9F);
+            txtdescription.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtdescription.Location = new Point(300, 315);
+            txtdescription.Margin = new Padding(3, 4, 3, 4);
+            txtdescription.Name = "txtdescription";
+            txtdescription.PlaceholderText = "";
+            txtdescription.SelectedText = "";
+            txtdescription.ShadowDecoration.CustomizableEdges = customizableEdges22;
+            txtdescription.Size = new Size(309, 92);
+            txtdescription.TabIndex = 6;
+            // 
+            // txtreference
+            // 
+            txtreference.BorderRadius = 7;
+            txtreference.CustomizableEdges = customizableEdges23;
+            txtreference.DefaultText = "";
+            txtreference.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtreference.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtreference.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtreference.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtreference.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtreference.Font = new Font("Segoe UI", 9F);
+            txtreference.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtreference.Location = new Point(300, 253);
+            txtreference.Margin = new Padding(3, 4, 3, 4);
+            txtreference.Name = "txtreference";
+            txtreference.PlaceholderText = "";
+            txtreference.SelectedText = "";
+            txtreference.ShadowDecoration.CustomizableEdges = customizableEdges24;
+            txtreference.Size = new Size(309, 36);
+            txtreference.TabIndex = 7;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 11F);
+            label6.Location = new Point(652, 235);
+            label6.Name = "label6";
+            label6.Size = new Size(85, 25);
+            label6.TabIndex = 9;
+            label6.Text = "Quantité";
+            label6.Click += label6_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 11F);
+            label7.Location = new Point(652, 292);
+            label7.Name = "label7";
+            label7.Size = new Size(106, 25);
+            label7.TabIndex = 10;
+            label7.Text = "Seuil alerte";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 11F);
+            label8.Location = new Point(650, 342);
+            label8.Name = "label8";
+            label8.Size = new Size(179, 25);
+            label8.TabIndex = 12;
+            label8.Text = "Date de Péremption";
+            // 
+            // txtprix
+            // 
+            txtprix.BorderRadius = 7;
+            txtprix.CustomizableEdges = customizableEdges25;
+            txtprix.DefaultText = "";
+            txtprix.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtprix.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtprix.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtprix.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtprix.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtprix.Font = new Font("Segoe UI", 9F);
+            txtprix.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtprix.Location = new Point(858, 165);
+            txtprix.Margin = new Padding(3, 4, 3, 4);
+            txtprix.Name = "txtprix";
+            txtprix.PlaceholderText = "";
+            txtprix.SelectedText = "";
+            txtprix.ShadowDecoration.CustomizableEdges = customizableEdges26;
+            txtprix.Size = new Size(309, 36);
+            txtprix.TabIndex = 13;
+            // 
+            // txtquantite
+            // 
+            txtquantite.BorderRadius = 7;
+            txtquantite.CustomizableEdges = customizableEdges27;
+            txtquantite.DefaultText = "";
+            txtquantite.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtquantite.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtquantite.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtquantite.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtquantite.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtquantite.Font = new Font("Segoe UI", 9F);
+            txtquantite.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtquantite.Location = new Point(858, 224);
+            txtquantite.Margin = new Padding(3, 4, 3, 4);
+            txtquantite.Name = "txtquantite";
+            txtquantite.PlaceholderText = "";
+            txtquantite.SelectedText = "";
+            txtquantite.ShadowDecoration.CustomizableEdges = customizableEdges28;
+            txtquantite.Size = new Size(309, 36);
+            txtquantite.TabIndex = 15;
+            // 
+            // txtseuil
+            // 
+            txtseuil.BorderRadius = 7;
+            txtseuil.CustomizableEdges = customizableEdges29;
+            txtseuil.DefaultText = "";
+            txtseuil.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            txtseuil.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            txtseuil.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            txtseuil.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            txtseuil.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtseuil.Font = new Font("Segoe UI", 9F);
+            txtseuil.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            txtseuil.Location = new Point(858, 281);
+            txtseuil.Margin = new Padding(3, 4, 3, 4);
+            txtseuil.Name = "txtseuil";
+            txtseuil.PlaceholderText = "";
+            txtseuil.SelectedText = "";
+            txtseuil.ShadowDecoration.CustomizableEdges = customizableEdges30;
+            txtseuil.Size = new Size(309, 36);
+            txtseuil.TabIndex = 16;
+            // 
+            // txtdate
+            // 
+            txtdate.Checked = true;
+            txtdate.CustomizableEdges = customizableEdges31;
+            txtdate.FillColor = Color.White;
+            txtdate.Font = new Font("Segoe UI", 9F);
+            txtdate.Format = DateTimePickerFormat.Long;
+            txtdate.Location = new Point(858, 342);
+            txtdate.MaxDate = new DateTime(9998, 12, 31, 0, 0, 0, 0);
+            txtdate.MinDate = new DateTime(1753, 1, 1, 0, 0, 0, 0);
+            txtdate.Name = "txtdate";
+            txtdate.ShadowDecoration.CustomizableEdges = customizableEdges32;
+            txtdate.Size = new Size(309, 34);
+            txtdate.TabIndex = 17;
+            txtdate.Value = new DateTime(2025, 11, 10, 15, 48, 31, 356);
+            // 
+            // Retour
+            // 
+            Retour.BorderRadius = 7;
+            Retour.CustomizableEdges = customizableEdges33;
+            Retour.DisabledState.BorderColor = Color.DarkGray;
+            Retour.DisabledState.CustomBorderColor = Color.DarkGray;
+            Retour.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            Retour.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            Retour.FillColor = SystemColors.ActiveCaption;
+            Retour.Font = new Font("Segoe UI", 9F);
+            Retour.ForeColor = Color.White;
+            Retour.Location = new Point(466, 480);
+            Retour.Name = "Retour";
+            Retour.ShadowDecoration.CustomizableEdges = customizableEdges34;
+            Retour.Size = new Size(177, 55);
+            Retour.TabIndex = 18;
+            Retour.Text = "Retour";
+            Retour.Click += Retour_Click;
+            // 
+            // Enregistrer
+            // 
+            Enregistrer.BorderRadius = 7;
+            Enregistrer.CustomizableEdges = customizableEdges35;
+            Enregistrer.DisabledState.BorderColor = Color.DarkGray;
+            Enregistrer.DisabledState.CustomBorderColor = Color.DarkGray;
+            Enregistrer.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            Enregistrer.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            Enregistrer.FillColor = SystemColors.ActiveCaption;
+            Enregistrer.Font = new Font("Segoe UI", 9F);
+            Enregistrer.ForeColor = Color.White;
+            Enregistrer.Location = new Point(697, 480);
+            Enregistrer.Name = "Enregistrer";
+            Enregistrer.ShadowDecoration.CustomizableEdges = customizableEdges36;
+            Enregistrer.Size = new Size(180, 55);
+            Enregistrer.TabIndex = 19;
+            Enregistrer.Text = "Enregistrer";
+            Enregistrer.Click += Enregistrer_Click;
+            // 
+            // labMessage
+            // 
+            labMessage.AutoSize = true;
+            labMessage.ForeColor = Color.LimeGreen;
+            labMessage.Location = new Point(593, 568);
+            labMessage.Name = "labMessage";
+            labMessage.Size = new Size(0, 20);
+            labMessage.TabIndex = 21;
+            // 
+            // AjouterMedicament
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1234, 656);
+            Controls.Add(labMessage);
+            Controls.Add(Enregistrer);
+            Controls.Add(Retour);
+            Controls.Add(txtdate);
+            Controls.Add(txtseuil);
+            Controls.Add(txtquantite);
+            Controls.Add(txtprix);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(txtreference);
+            Controls.Add(txtdescription);
+            Controls.Add(txtnom);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "AjouterMedicament";
+            Text = "AjouterMedicament";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Guna.UI2.WinForms.Guna2TextBox txtnom;
+        private Guna.UI2.WinForms.Guna2TextBox txtdescription;
+        private Guna.UI2.WinForms.Guna2TextBox txtreference;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Guna.UI2.WinForms.Guna2TextBox txtprix;
+        private Guna.UI2.WinForms.Guna2TextBox txtquantite;
+        private Guna.UI2.WinForms.Guna2TextBox txtseuil;
+        private Guna.UI2.WinForms.Guna2DateTimePicker txtdate;
+        private Guna.UI2.WinForms.Guna2Button Retour;
+        private Guna.UI2.WinForms.Guna2Button Enregistrer;
+        private Label labMessage;
+    }
+}
---
+++ GestionPharmacie/AjouterMedicament.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestionPharmacie
+{
+    public partial class AjouterMedicament : Form
+    {
+        public AjouterMedicament()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Retour_Click(object sender, EventArgs e)
+        {
+            Medicaments medicaments = new Medicaments();
+            medicaments.Show();
+            this.Hide();
+        }
+
+        private void Enregistrer_Click(object sender, EventArgs e)
+        {
+            // Vérification minimale
+            if (txtreference.Text == "" || txtnom.Text == "" || txtquantite.Text == "" || txtprix.Text == ""
+                || txtseuil.Text == "" || txtdate.Text == "" || txtdescription.Text == "") { 
+                labMessage.Text = "Veuillez remplir tous les champs.";
+            }
+            Medicament med = new Medicament();
+
+            med.Reference = txtreference.Text.Trim();
+            med.Nom = txtnom.Text.Trim();
+            med.Description = txtdescription.Text.Trim();
+            med.Prix = decimal.Parse(txtprix.Text.Trim());
+            med.Quantite = int.Parse(txtquantite.Text.Trim());
+            med.SeuilAlerte = int.Parse(txtseuil.Text.Trim());
+            med.DatePeremption = txtdate.Value.Date;
+
+            bool resultat = med.Ajouter();
+
+            if (resultat)
+            {
+                labMessage.Text = "Médicament ajouté avec succès.";
+
+                // Réinitialiser le formulaire après ajout
+                txtreference.Clear();
+                txtnom.Clear();
+                txtdescription.Clear();
+                txtprix.Clear();
+                txtquantite.Clear();
+                txtseuil.Clear();
+                txtdate.Value = DateTime.Now;
+            }
+            else
+            {
+                labMessage.Text= "Erreur lors de l'ajout du médicament.";
+            }
+        }
+    }
+}
